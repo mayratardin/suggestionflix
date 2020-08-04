eee94876bf659269caa1c2137cd5f1551033f3f5
mayratardin <63374804+mayratardin@users.noreply.github.com>
2020-08-03 21:37:57 -0300

    Iniciando o formulário e Eslint

---
+++ src/components/FormField/index.js
@@ -0,0 +1,28 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+
+function FormField({
+  label, type, name, value, onChange,
+}) {
+  const fieldId = `id_${name}`;
+
+  return (
+    <div>
+      <label
+        htmlFor={fieldId}
+      >
+        {label}
+        :
+        <input
+          id={fieldId}
+          type={type}
+          value={value}
+          name={name}
+          onChange={onChange}
+        />
+      </label>
+    </div>
+  );
+}
+
+export default FormField;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,18 +1,114 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+    const valoresIniciais = {
+      nome: '',
+      descricao: '',
+      cor: '',
+    }
+    const [categorias, setCategorias] = useState([]);
+    const [valor, setValores] = useState(valoresIniciais);
+  
+  
+    function setValor(chave, valor) {
+      // chave: nome, descricao, bla, bli
+      setValores({
+        ...valor,
+        [chave]: valor, // nome: 'valor'
+      })
+    }
+  
+    function handleChange(infosDoEvento) {
+      setValor(
+        infosDoEvento.target.getAttribute('name'),
+        infosDoEvento.target.value
+      );
+    }
+  
     return (
-        <PageDefault>
-            <h1>Cadastro de Categoria</h1>
-
-            <Link to="/">
-                Ir para Home
-            </Link>
-
-        </PageDefault>
+      <PageDefault>
+        <h1>Cadastro de Categoria: {valor.nome}</h1>
+  
+        <form onSubmit={function handleSubmit(infosDoEvento) {
+            infosDoEvento.preventDefault();
+            setCategorias([
+              ...categorias,
+              valor
+            ]);
+  
+            setValores(valoresIniciais)
+        }}>
+  
+          <FormField
+            label="Nome da Categoria"
+            type="text"
+            name="nome"
+            value={valor.nome}
+            onChange={handleChange}
+          />
+  
+          <FormField
+            label="Descrição"
+            type="????"
+            name="descricao"
+            value={valor.descricao}
+            onChange={handleChange}
+          />
+          {/* <div>
+            <label>
+              Descrição:
+              <textarea
+                type="text"
+                value={valor.descricao}
+                name="descricao"
+                onChange={handleChange}
+              />
+            </label>
+          </div> */}
+  
+          <FormField
+            label="Cor"
+            type="color"
+            name="cor"
+            value={valor.cor}
+            onChange={handleChange}
+          />
+          {/* <div>
+            <label>
+              Cor:
+              <input
+                type="color"
+                value={valor.cor}
+                name="cor"
+                onChange={handleChange}
+              />
+            </label>
+          </div> */}
+  
+          <button>
+            Cadastrar
+          </button>
+        </form>
+        
+  
+        <ul>
+          {categorias.map((categoria, indice) => {
+            return (
+              <li key={`${categoria}${indice}`}>
+                {categoria.nome}
+              </li>
+            )
+          })}
+        </ul>
+  
+        <Link to="/">
+          Ir para home
+        </Link>
+      </PageDefault>
     )
-}
-
-export default CadastroCategoria;
+  }
+  
+  export default CadastroCategoria;
